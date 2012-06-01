eb3f0a1a4412c6c30e9ae7d85374e86b71511a73
Luis Felipe Perez <slashcat@gmail.com>
2012-06-01 14:25:52 -0500

    Bootstrap

---
+++ application/views/template.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Eventos Nacionales</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="/assets/css/bootstrap.css" rel="stylesheet">
+	<link href="/assets/css/asmac.css" rel="stylesheet">
+	<link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!-- Barra de navegación -->
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+
+				<a href="#" class="brand"><span>Scouts</span></a>
+
+				<div class="btn-group pull-right">
+					<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="icon-user"></i> Username
+						<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Perfil</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Cerrar Sesión</a></li>
+					</ul>
+				</div>
+				
+				<div class="nav-collapse">
+					<ul class="nav">
+						<li><a href="#" title=""><i class="icon-dash icon-light-purple"></i>  Panel</a></li>
+						<li class="divider-vertical"></li>		
+						<li><a href="#" title=""><i class="icon-check icon-light-purple"></i> Registro</a></li>
+						<li class="divider-vertical"></li>
+						<li><a href="#" title=""><i class="icon-nameplate icon-light-purple"></i> Staff</a></li>
+						<li class="divider-vertical"></li>
+						<li><a href="#" title=""><i class="icon-flag icon-light-purple"></i>  Campos</a></li>
+						<li class="divider-vertical"></li>
+						<li><a href="#" title=""><i class="icon-users icon-light-purple"></i>  Membresía</a></li>
+						<li class="divider-vertical"></li>
+						<li><a href="#" title=""><i class="icon-cog icon-light-purple"></i>   Configuración</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Termina barra de navegación -->
+
+	<!-- Area de contenido -->
+	<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span3">
+					<div class="well sidebar-nav">						
+						<ul class="nav nav-list">
+							<li class="nav-header">Accesos Rápidos</li>
+							<li><a href="#">Nuevo Equipo</a></li>
+							<li><a href="#">Equipo Preregistrado</a></li>
+							<li><a href="#">Configuraciones</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="span9">
+					<h1>Panel Principal</h1>
+
+				</div>
+			</div>
+	</div>
+	<!-- Termina area de contenido -->
+
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="/assets/js/bootstrap.js"></script>
+</body>
+</html>
