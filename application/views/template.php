149b474c396007c3e1ac32efc2151bec94edb909
Luis Felipe Perez <slashcat@gmail.com>
2012-06-04 16:04:24 -0500

    Responsive UI

--- application/views/template.php
+++ application/views/template.php
@@ -15,7 +15,7 @@
 	<!-- Barra de navegación -->
 	<div class="navbar navbar-fixed-top">
 		<div class="navbar-inner">
-			<div class="container-fluid">
+			<div class="container">
 				
 				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
 					<span class="icon-bar"></span>
@@ -26,18 +26,18 @@
 				<a href="#" class="brand"><span>Scouts</span></a>
 
 				<div class="btn-group pull-right">
-					<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-						<i class="icon-user"></i> Username
+					<a class="btn btn-logout dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="icon-user"></i> Cuenta
 						<span class="caret"></span>
 					</a>
 					<ul class="dropdown-menu">
-						<li><a href="#">Perfil</a></li>
+						<li><a href="#">Editar Perfil</a></li>
 						<li class="divider"></li>
 						<li><a href="#">Cerrar Sesión</a></li>
 					</ul>
 				</div>
 				
-				<div class="nav-collapse">
+				<nav class="nav-collapse">
 					<ul class="nav">
 						<li><a href="#" title=""><i class="icon-dash icon-light-purple"></i>  Panel</a></li>
 						<li class="divider-vertical"></li>		
@@ -51,36 +51,50 @@
 						<li class="divider-vertical"></li>
 						<li><a href="#" title=""><i class="icon-cog icon-light-purple"></i>   Configuración</a></li>
 					</ul>
-				</div>
+				</nav>
 			</div>
 		</div>
 	</div>
 	<!-- Termina barra de navegación -->
 
 	<!-- Area de contenido -->
-	<div class="container-fluid">
-			<div class="row-fluid">
-				<div class="span3">
-					<div class="well sidebar-nav">						
-						<ul class="nav nav-list">
-							<li class="nav-header">Accesos Rápidos</li>
-							<li><a href="#">Nuevo Equipo</a></li>
-							<li><a href="#">Equipo Preregistrado</a></li>
-							<li><a href="#">Configuraciones</a></li>
-						</ul>
+	<div class="container">
+		<div class="row-fluid">
+			<div class="page-header-top span12">
+				<h1>Panel de Inicio</h1>
+			</div>
+		</div>
+
+		<div class="row-fluid">
+			<aside class="span3">					
+				<ul id="side-nav" class="nav nav-tabs nav-stacked">
+					<li><a href="#"><i class="icon-check"></i> Nuevo Equipo</a></li>
+					<li><a href="#"><i class="icon-list"></i> <span class="hidden-tablet">Equipo</span> Pre-Registrado</a></li>
+					<li><a href="#"><i class="icon-cog"></i> Configuraciones</a></li>
+				</ul>
+			</aside>
+
+			<div class="span9">
+				<div class="widget">
+					<div class="widget-header">
+						<h3>Módulos</h3>
+					</div>
+					<div class="widget-content">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 					</div>
 				</div>
-
-				<div class="span9">
-					<h1>Panel Principal</h1>
-
-				</div>
 			</div>
+		</div>
 	</div>
 	<!-- Termina area de contenido -->
 
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script src="/assets/js/bootstrap.js"></script>
+	<script src="/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
